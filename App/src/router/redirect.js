b8a49364a4729305c7dfbe9885cd195d59dd082e
SergioTrinquet <serge.trinquet@gmail.com>
2021-02-13 02:30:59 +0100

    Modif suite à àchgmt nom Getters

--- App/src/router/redirect.js
+++ App/src/router/redirect.js
@@ -3,12 +3,12 @@ import store from '../store'
 export default (path, next) => {
     // Si le currentUser a les droits d'accès à la page...
     const authorizedPath = store.state.pages.find(i => {
-        return i.routeName == path && i.roles.indexOf(store.getters.currentUser.role) > -1;
+        return i.routeName == path && i.roles.indexOf(store.getters.getCurrentUser.role) > -1;
     });
     if(typeof authorizedPath !== 'undefined') {
         next();
     } else {
-        alert(`Vous avez le rôle de ${store.getters.currentUser.role}.\nVous n'avez pas les droits d'accès à cette page ("${path}").\nVous allez être redirigé vers la page d'accueil.`);
+        alert(`Vous avez le rôle de ${store.getters.getCurrentUser.role}.\nVous n'avez pas les droits d'accès à cette page ("${path}").\nVous allez être redirigé vers la page d'accueil.`);
         next({ name: "accueil" }); // redirection vers page d'accueil
     }
 } 
